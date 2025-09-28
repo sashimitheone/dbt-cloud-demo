568ae4a75225ca5831bf63dd4f40955db0b5d414
Alexandru Stajila <stajilaalex@gmail.com>
2025-09-28 15:25:21 +0000

    renamed src files with an "_" prefix

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -2,7 +2,7 @@ with customers as (
     select * from {{ ref ('stg_jaffle_shop__customers')}}
 ),
 orders as (
-    select * from {{ ref ('stg_jaffle_shop__orders')}} -- I guess this one should be replaced by ref('stg_jaffle_shop__orders')
+    select * from {{ ref ('fct_orders')}} -- before it was refferencing stg_jaffle_shop__orders. 
 ),
 customer_orders as (
     select
