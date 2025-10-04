645537ff3f62e9eeb476aecb7add77adbfd596bc
Alexandru Stajila <stajilaalex@gmail.com>
2025-10-04 08:10:41 +0000

    Adding model and column level documentation in stg_jaffle_shop.yml file

--- tests/assert_stg_stripe__payments_total_positive.sql
+++ tests/assert_stg_stripe__payments_total_positive.sql
@@ -2,9 +2,9 @@
 
 
 /* 
-Singular test: we check wether total value per order id is always >=0.
+Singular test assertion: Refunds have a negative amount, so the total amount should always be >= 0.
 Goal is to engineer a test that returns a row for every data point  that violates your assertion. 
-Therefore qyery should return records where total_amount<0. If query returns at least 1 row  the test will fail.
+Therefore query should return records where total_amount<0. If query returns at least 1 row  the test will fail.
 */
 
 select
